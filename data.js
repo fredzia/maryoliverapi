73d3ec5f14840f613c57190f24bc1078a7d6311f
fredzia <fredziaga@gmail.com>
2020-03-22 20:17:17 -0300

    Implementa primera versión andando de la api

--- data.js
+++
@@ -1,7 +0,0 @@
-//module.exports exposes this array to other files
-
-module.exports = [
-    { "id": "tt0110357", "name": "The Lion King", "genre": "animation"},
-    { "id": "tt0068646", "name": "The Godfather", "genre": "crime"},
-    { "id": "tt0468569", "name": "The Dark Knight", "genre": "action"},
-];
