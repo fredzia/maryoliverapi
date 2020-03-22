0933b80f675a73dd982daeefb554fb96c7413b1f
fredzia <fredziaga@gmail.com>
2020-03-22 20:34:21 -0300

    Agrega primera frase en /poema

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get("/poema", (req, res) => {
+    return res.json(["I don't want to end up simply having visited this world."]);
+ });
 
 app.listen(port, () => {
     console.log(`app listening at ${port}`);
