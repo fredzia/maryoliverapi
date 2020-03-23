7090f48e172f6cf6fad86dc4eec7de6aaee2c5be
fredzia <fredziaga@gmail.com>
2020-03-22 21:45:09 -0300

    Obtiene poema de data.js por id

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+let data = require('./data');
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get("/poema", (req, res) => {
-    return res.json(["I don't want to end up simply having visited this world."]);
+app.get('/poemas/:id', (req, res) => {
+    const id = req.params.id;
+    const poema = data.find(_poema => _poema.id === id);
+
+    if (poema) {
+        res.json(poema);
+     } else {
+        res.json({ message: `poema ${id} no existe :(`})
+     }
  });
 
 app.listen(port, () => {
