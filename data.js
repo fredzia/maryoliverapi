6e78bd4fb245b615a769c3cca99335359f4dfff6
fredzia <fredziaga@gmail.com>
2020-03-22 21:44:40 -0300

    Agrega data.js con array de 3 poemas

---
+++ data.js
@@ -0,0 +1,5 @@
+module.exports = [
+    {"id": "1", "frase": "I don't want to end up simply having visited this world."},
+    {"id": "2", "frase": "It is better for the heart to break, than not to break."},
+    {"id": "3", "frase": "Said the river: imagine everything you can imagine, then keep on going."},
+]
